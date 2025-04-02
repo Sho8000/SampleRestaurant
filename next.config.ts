57ce73b221b57a726973eee8c9f14cc72d41bbdd
Sho8000 <yoshikichi8000@gmail.com>
2025-04-02 02:15:48 -0700

    changed nextconfig file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Your Next.js config options here */
 };
 
-export default nextConfig;
+module.exports = {
+  ...nextConfig, // Spread the Next.js config options
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
