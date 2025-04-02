dc4063eba09f67dd6739570b81cfd9ceb84b0dc8
Sho8000 <yoshikichi8000@gmail.com>
2025-04-02 01:59:43 -0700

    save for deploy and fix lint error

---
+++ src/app/(components)/FeaturedDishes/FeacuredDishes.tsx
@@ -0,0 +1,10 @@
+export default function FeaturedDishes() {
+  return (
+    <section className="w-[100%] min-h-[80vh] relative">
+      
+    </section>
+  );
+}
+
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
+//import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import Hero from "./(components)/Hero/Hero/Hero";
+//import client from "./lib/contentful";
+//import { RecipeProps } from "./lib/contentfulInterface";
 
-export default function Home() {
+export default async function Home() {
+/* 
+
+  const fetchEntries = async () => {
+    try{
+      const entries = await client.getEntries({content_type: "recipes"})
+      return entries.items;
+    } catch(error){
+      console.log('error fetching entries:', error)
+    }
+  }
+  const entries:any[]|undefined = await fetchEntries();
+  if(entries){
+    console.log("entries: ", entries);
+    console.log("title: ", entries[0].fields.recipeName);
+    console.log("Image: ", "https:"+entries[0].fields.recipeImg.fields.file.url);
+    console.log("Desc: ", entries[0].fields.recipeDescription.content);
+    console.log("Price: ", entries[0].fields.price)
+    console.log("Category: ", entries[0].fields.category)
+    console.log("Ingredients: ", entries[0].fields.ingredients)    
+  }
+//  console.dir(entries, {depth:null})
+ */
   return (
     <div>
       <Hero/>
+{/*       {documentToReactComponents(entries[0].fields.category)} */}
     </div>
   );
 }
