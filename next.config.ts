f877cc56bf1239313e12db7b89f4010d293c9aaa
Sho8000 <yoshikichi8000@gmail.com>
2025-04-02 02:18:36 -0700

    delete eslint for typescript

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* Your Next.js config options here */
+  /* config options here */
 };
 
-module.exports = {
-  ...nextConfig, // Spread the Next.js config options
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-};
+export default nextConfig;
