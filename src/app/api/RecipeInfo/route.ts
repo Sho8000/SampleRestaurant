a12536b836f4283582d02eedf53908c473b030e3
Sho8000 <yoshikichi8000@gmail.com>
2025-04-03 14:53:20 -0700

    fixed router

--- src/app/api/RecipeInfo/route.ts
+++ src/app/api/RecipeInfo/route.ts
@@ -1,8 +1,6 @@
-import { Asset, Entry, EntrySkeletonType } from "contentful";
-import { Document as RichTextDocument } from '@contentful/rich-text-types'
 import client from "@/app/lib/contentful";
 
-const getRecipe = async () => {
+export const getRecipe = async () => {
     try{
       const entries = await client.getEntries({content_type: "recipes"})
       return entries.items;
@@ -10,25 +8,4 @@ const getRecipe = async () => {
       console.log('error fetching entries:', error)
     }
   }
-
-export const allRecipe = async () => {
-    const data:object[] = []
-    const entries:Entry<EntrySkeletonType, undefined, string>[]|undefined = await getRecipe();
-    if(entries){
-      entries.map((item)=>{
-        const cmsImage = item.fields.recipeImg as Asset
-        const cmsDescription = item.fields.recipeDescription as RichTextDocument
-
-        data.push({
-          recipeName:item.fields.recipeName,
-          recipeImage:"https"+cmsImage.fields.file?.url,
-          recipeDescription:cmsDescription.content,
-          recipePrice:item.fields.price,
-          recipeCategory:item.fields.category,
-          recipeIngredients:item.fields.ingredients,
-        })
-      })
-    }
-    return data
-}
   
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
 "use client"
 import { useEffect } from "react";
 import Hero from "./(components)/Hero/Hero/Hero";
-import { allRecipe } from "./api/RecipeInfo/route";
+import { getRecipe } from "./api/RecipeInfo/route";
+import { Asset, Entry, EntrySkeletonType } from "contentful";
+import { Document as RichTextDocument } from '@contentful/rich-text-types'
 
 export default function Home() {
   useEffect(()=>{
     const getData = async () => {
-      const data = await allRecipe()
-      console.log("This is Recipe Data",data)
-      
+      const items:object[] = []
+      const entries:Entry<EntrySkeletonType, undefined, string>[]|undefined = await getRecipe();
+      if(entries){
+        entries.map((item)=>{
+          const cmsImage = item.fields.recipeImg as Asset
+          const cmsDescription = item.fields.recipeDescription as RichTextDocument
+  
+          items.push({
+            recipeName:item.fields.recipeName,
+            recipeImage:"https"+cmsImage.fields.file?.url,
+            recipeDescription:cmsDescription.content,
+            recipePrice:item.fields.price,
+            recipeCategory:item.fields.category,
+            recipeIngredients:item.fields.ingredients,
+          })
+        })
+      }
+      console.log("This is Recipe Data",items)
     }
     getData()
   },[])
